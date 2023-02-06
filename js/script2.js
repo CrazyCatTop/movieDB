9ef22349bb4f12f6fc38b24f6528c2f911028616
Cat <daniliukdaniil@gmail.com>
2023-02-06 19:19:57 +0300

    6th commit

--- js/script2.js
+++ js/script2.js
@@ -14,7 +14,11 @@ const promo = document.querySelector('.promo'),
       ads = promo.querySelectorAll('.promo__adv img'),
       genre = promo.querySelector('.promo__genre'),
       background = promo.querySelector('.promo__bg'),
-      movieItems = promo.querySelectorAll('.promo__interactive-item');
+      movieList = promo.querySelector('.promo__interactive-list'),
+      form = promo.querySelector('.add'),
+      input = form.querySelector('.adding__input'),
+      checkbox = form.querySelector('[type="checkbox"]'),
+      button = form.querySelector('button');
 
 ads.forEach((img) => {
     img.remove();
@@ -22,10 +26,47 @@ ads.forEach((img) => {
 
 genre.textContent = 'драма';
 
-background.style.background = 'url("/img/bg.jpg") center 70% / cover no-repeat';
+background.style.background = 'url("img/bg.jpg") center 70% / cover no-repeat';
 
-movieDB.movies.sort(); 
+moviesUpdate();
 
-movieItems.forEach((movie, i) => {
-    movie.innerHTML = `${i + 1}. ${movieDB.movies[i]} <div class="delete"></div>`;
+button.addEventListener('click', () => {
+    let film = ('' + input.value).trim();
+    film = film[0].toUpperCase() + film.substring(1);
+    
+    input.value = '';
+    
+    if (film !== '') {
+        if (film.length > 21) {
+            film = film.substring(0, 21) + '...';
+        }
+        
+        if (checkbox.checked) {
+            alert('Добавляем любимый фальм');
+            checkbox.checked = false;
+        }
+    
+        movieDB.movies.push(film);
+        moviesUpdate();
+    } else {
+        alert('Вы не ввели название фильма');
+    }
 });
+
+function moviesUpdate() {
+    movieDB.movies.sort(); 
+
+    movieList.innerHTML = '';
+
+    for (let i = 0; i < movieDB.movies.length; i++) {
+        movieList.innerHTML += `<li class="promo__interactive-item">${i + 1}. ${movieDB.movies[i]}<div class="delete"></div></li>`;
+    }
+
+    promo.querySelectorAll('.delete').forEach((trashcan, i) => {
+        trashcan.addEventListener('click', () => {
+            movieDB.movies.splice(i, 1);
+            moviesUpdate();
+        });
+        console.log(movieDB.movies);
+    });
+}
